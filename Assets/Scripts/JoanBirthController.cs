a0e42a97e31ab3070d79f18ce654cd53c0a71770
jgairaud <julian.gairaud1010@gmail.com>
2016-02-20 18:16:50 -0500

    Adding Joan's Birth House into complete scene.

---
+++ Assets/Scripts/JoanBirthController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoanBirthController : MonoBehaviour {
+
+	public AudioSource audio;
+	public GameObject myself;
+	public AudioClip clip;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	//void OnCollisionEnter(Collision collision) {
+	void OnTriggerEnter(Collider other){
+        
+		audio.clip = clip;
+        audio.Play();
+		myself.SetActive(false);
+        
+    }
+}
