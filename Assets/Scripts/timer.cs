fe36491b90abe16213769e31a205ce775f7aed49
vikram0602 <vikram0602@gmail.com>
2016-02-21 10:48:10 -0500

    added time travel
    press t for magic in charscene

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+public class timer : MonoBehaviour {
+	private float timer1 = 5.0f;
+	public string nextScene; 	 	// Use this for initialization 	
+	void Start () 
+	{ 	
+
+	} 	 	// Update is called once per frame 	
+	void Update () 
+	{ 	
+		timer1 -= Time.deltaTime * 1; 
+		if(timer1 <= 0.0f) 		
+			Application.LoadLevel("CompleteScene"); 
+	} 
+}
---
+++ Assets/Scripts/transport.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transport : MonoBehaviour {
+	public GameObject trans;
+
+	public AudioClip myclip;
+	public timer obj;
+	// Update is called once per frame
+
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.T)) {
+			
+			trans.SetActive (true);
+			this.gameObject.AddComponent<AudioSource> ();
+			this.GetComponent<AudioSource> ().clip = myclip;
+			this.GetComponent<AudioSource> ().Play ();
+			//obj.enabled = true;
+			this.GetComponent<timer>().enabled=true;
+
+
+		}
+	}
+
+
+
+
+			
+	
+	}
+
