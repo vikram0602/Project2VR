c0ce2b971b65d4d10aa42dd15a724612dea8630b
jgairaud <julian.gairaud1010@gmail.com>
2016-02-21 22:55:58 -0500

    Audio Triggers for Military and Messenger of God Scenes adde, I also added bounding planes for the first part of the terrain (before entering the village)

---
+++ Assets/MIlitaryController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MIlitaryController : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource audio;
+	public GameObject Myself;
+	public GameObject Myself2;
+	public GameObject Myself3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	 void OnTriggerEnter(Collider other){
+        
+		audio.clip = clip;
+        audio.Play();
+		
+		Myself2.SetActive(false);
+		Myself3.SetActive(false);
+		Myself.SetActive(false);
+    }
+}
---
+++ Assets/MessengerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessengerController : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource audio;
+	public GameObject Myself;
+	public GameObject Myself2;
+	void Start () {
+	
+	}
+	
+	 void OnTriggerEnter(Collider other){
+        
+		audio.clip = clip;
+        audio.Play();
+		
+		Myself2.SetActive(false);
+		Myself.SetActive(false);
+    }
+}
