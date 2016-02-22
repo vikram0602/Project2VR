6d04aafe4118c94a7214bf08f7037facf1197092
vikram0602 <vikram0602@gmail.com>
2016-02-21 20:37:45 -0500

    animation on trigger added

--- Assets/ExecutionController.cs
+++ Assets/ExecutionController.cs
@@ -6,17 +6,18 @@ public class ExecutionController : MonoBehaviour {
 	public AudioClip clip;
 	public AudioSource audio;
 	public	GameObject Myself;
-	
+	public GameObject joan;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	void OnTriggerEnter(Collider other){
+	 void OnTriggerEnter(Collider other){
         
 		audio.clip = clip;
         audio.Play();
 		Myself.SetActive(false);
-        
+		//triggerAnim.triggerAnimation ();
+		joan.GetComponent<Animator>().enabled=true;
     }
 }
