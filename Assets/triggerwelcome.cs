3d48663bd10983a8a41b3880c0544d91e5b59c92
vikram0602 <vikram0602@gmail.com>
2016-02-23 05:17:05 -0500

    added villager nd soldier for welcome

---
+++ Assets/triggerwelcome.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerwelcome : MonoBehaviour {
+
+	public AudioClip clip;
+	public AudioSource audio;
+	public	GameObject Myself;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		audio.clip = clip;
+		audio.Play();
+		Myself.SetActive(false);
+
+
+	}
+}
